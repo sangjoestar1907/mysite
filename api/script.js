5f1b17a103a354b2663f0be0d0b8d03c68dce3f4
sangjoestar1907 <cheesesato1@gmail.com>
2025-09-03 22:29:43 +0700

    Delete api directory

--- api/script.js
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.send(`
-    print(" Script từ API động đã chạy thành công!")
-    game.Players.LocalPlayer:Kick("Script test đã hoạt động 🚀")
-  `);
-}
