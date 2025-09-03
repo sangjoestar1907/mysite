b117663d5082bceca2eb0cfef1566b73da230f90
sangjoestar1907 <cheesesato1@gmail.com>
2025-09-03 22:05:51 +0700

    Update script.js

--- api/script.js
+++ api/script.js
@@ -1,1 +1,8 @@
 
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.send(`
+    print(" Script từ API động đã chạy thành công!")
+    game.Players.LocalPlayer:Kick("Script test đã hoạt động 🚀")
+  `);
+}
