07a491218f759b7b96b60cc4df5402279216f72b
sangjoestar1907 <cheesesato1@gmail.com>
2025-09-04 21:36:55 +0700

    Update script.js

--- api/script.js
+++ api/script.js
@@ -1,18 +1,1 @@
-export default function handler(req, res) {
-  const ua = req.headers["user-agent"] || "";
 
-  // Nếu không phải Roblox thì chặn
-  if (!ua.includes("Roblox")) {
-    res.status(403).send("Are You Skider?");
-    return;
-  }
-
-  // Code script Roblox của bạn
-  const script = `
-      print("Hello từ API riêng của bạn!")
-      -- Đây là chỗ bạn bỏ code UI, auto farm, ...
-  `;
-
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(script);
-}
